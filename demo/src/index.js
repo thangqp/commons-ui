dea91db61638bc5944a05ae910c065175e35706d
Mika Theodore <michael.theodore@rte-france.com>
2020-04-29 10:07:42 +0200

    First step to add TopBar as a first common component thanks to nwb

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 
 import Example from '../../src'
+import TopBar from '../../src/components/TopBar'
 
 export default class Demo extends Component {
   render() {
@@ -11,5 +12,12 @@ export default class Demo extends Component {
     </div>
   }
 }
+
+// EXAMPLE FOR THE TOPBAR
+/*export default class Demo {
+  render() {
+    <TopBar onParametersClick={() => console.log("settings")} onLogoutClick={() => console.log("logout")} />
+  }
+}*/
 
 render(<Demo/>, document.querySelector('#demo'))
