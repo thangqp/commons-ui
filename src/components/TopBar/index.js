873aa5f78df316c87f57d9587e6d866679e374b2
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-06-16 13:36:34 +0200

    export TopBar version 0
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -5,4 +5,239 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-export { default } from './top-bar';
+import React, {useRef, useState} from "react";
+
+import {FormattedMessage} from "react-intl";
+
+import AppBar from "@material-ui/core/AppBar";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+import {makeStyles} from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import SettingsIcon from '@material-ui/icons/Settings';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import { withStyles } from '@material-ui/core/styles';
+
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AppsIcon from '@material-ui/icons/Apps';
+import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
+
+import PropTypes from "prop-types";
+import {useSelector} from "react-redux";
+import FullscreenIcon from '@material-ui/icons/Fullscreen';
+import FullScreen, {fullScreenSupported} from "react-request-fullscreen";
+
+const useStyles = makeStyles(() => ({
+    grow: {
+        flexGrow: 1,
+    },
+    logo: {
+        width: 48,
+        height: 48,
+        cursor: 'pointer'
+    },
+    menuIcon: {
+        width: 24,
+        height: 24,
+    },
+    title: {
+        marginLeft: 18,
+        cursor: 'pointer'
+    }
+}));
+
+const StyledMenu = withStyles({
+    paper: {
+        border: '1px solid #d3d4d5',
+    },})(props => (
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        {...props}
+    />
+));
+
+const StyledMenuItem = withStyles(theme => ({
+    root: {
+        '&:focus': {
+            backgroundColor: theme.palette.primary.main,
+            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+                color: theme.palette.common.white,
+            },
+        },
+    },
+}))(MenuItem);
+
+const TopBar = (props) => {
+
+    const user = useSelector(state => state.user);
+
+    const classes = useStyles();
+
+    const [anchorElGeneralMenu, setAnchorElGeneralMenu] = React.useState(null);
+
+    const [anchorElAppsMenu, setAnchorElAppsMenu] = React.useState(null);
+
+    const fullScreenRef = useRef(null);
+
+    const [isFullScreen, setIsFullScreen] = useState(false);
+
+    //const history = useHistory();
+
+    const handleClickGeneralMenu = event => {
+        setAnchorElGeneralMenu(event.currentTarget);
+    };
+
+    const handleCloseGeneralMenu = () => {
+        setAnchorElGeneralMenu(null);
+    };
+    const handleClickAppsMenu = event => {
+        setAnchorElAppsMenu(event.currentTarget);
+    };
+
+    const handleCloseAppsMenu = () => {
+        setAnchorElAppsMenu(null);
+    };
+
+    const onParametersClick = () => {
+        handleCloseGeneralMenu();
+        if (props.onParametersClick) {
+            props.onParametersClick();
+        }
+    };
+
+    const onLogoClick = () => {
+        handleCloseAppsMenu();
+        //history.replace("/");
+    };
+
+    function onFullScreenChange (isFullScreen) {
+        setIsFullScreen(isFullScreen);
+    }
+
+    function requestOrExitFullScreen () {
+        setAnchorElGeneralMenu(null);
+        fullScreenRef.current.fullScreen();
+    }
+
+    return (
+        <AppBar position="static" color="default" className={classes.appBar}>
+            <FullScreen ref={fullScreenRef} onFullScreenChange={onFullScreenChange} onFullScreenError={(e) => console.debug("full screen error : " + e.message)}>
+            </FullScreen>
+            <Toolbar>
+                <h2>Logo</h2>
+                <Typography variant="h6" className={classes.title} onClick={onLogoClick}>
+                    Study app
+                </Typography>
+                <div className={classes.grow} />
+
+                {user && (
+                    <div>
+                        <Button
+                            aria-controls="apps-menu"
+                            aria-haspopup="true"
+                            onClick={handleClickAppsMenu}
+                        >
+                            <AppsIcon/>
+                        </Button>
+
+                        <StyledMenu
+                            id="apps-menu"
+                            anchorEl={anchorElAppsMenu}
+                            keepMounted
+                            open={Boolean(anchorElAppsMenu)}
+                            onClose={handleCloseAppsMenu}
+                        >
+                            <StyledMenuItem onClick={onLogoClick}>
+                                <ListItemIcon>
+                                    <h2>Logo</h2>
+                                </ListItemIcon>
+                                <ListItemText >
+                                    Study app
+                                </ListItemText>
+                            </StyledMenuItem>
+                        </StyledMenu>
+                    </div>
+                )}
+
+                <h3>{user !== null ? user.profile.name : ""}</h3>
+
+                {user && (
+                    <div>
+                        <Button
+                            aria-controls="general-menu"
+                            aria-haspopup="true"
+                            onClick={handleClickGeneralMenu}
+                        >
+                            <MenuIcon/>
+                        </Button>
+
+                        <StyledMenu
+                            id="general-menu"
+                            anchorEl={anchorElGeneralMenu}
+                            keepMounted
+                            open={Boolean(anchorElGeneralMenu)}
+                            onClose={handleCloseGeneralMenu}
+                        >
+                            <StyledMenuItem onClick={onParametersClick}>
+                                <ListItemIcon>
+                                    <SettingsIcon fontSize="small" />
+                                </ListItemIcon>
+                                <ListItemText>
+                                    <FormattedMessage id="settings"/>
+                                </ListItemText>
+                            </StyledMenuItem>
+                            {
+                                fullScreenSupported() ?  (
+                                    <StyledMenuItem onClick={requestOrExitFullScreen}>
+                                        {
+                                            isFullScreen ? (<>
+                                                <ListItemIcon>
+                                                    <FullscreenExitIcon fontSize="small" />
+                                                </ListItemIcon>
+                                                <ListItemText>
+                                                    <FormattedMessage id="exitFullScreen"/>
+                                                </ListItemText> </>) : (<>
+                                                <ListItemIcon>
+                                                    <FullscreenIcon fontSize="small" />
+                                                </ListItemIcon>
+                                                <ListItemText>
+                                                    <FormattedMessage id="goFullScreen"/>
+                                                </ListItemText></>)
+                                        }
+                                    </StyledMenuItem>) : <></>
+                            }
+                            <StyledMenuItem onClick={props.onLogoutClick}>
+                                <ListItemIcon>
+                                    <ExitToAppIcon fontSize="small" />
+                                </ListItemIcon>
+                                <ListItemText >
+                                    <FormattedMessage id="logout"/>
+                                </ListItemText>
+                            </StyledMenuItem>
+                        </StyledMenu>
+                    </div>
+                )}
+            </Toolbar>
+        </AppBar>
+    )
+};
+
+TopBar.propTypes = {
+    onParametersClick: PropTypes.func
+};
+
+export default TopBar;
