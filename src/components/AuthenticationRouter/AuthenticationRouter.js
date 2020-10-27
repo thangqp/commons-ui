thangqp/rte-gridsuite-commons-ui

commit f2f06ea375646e6bde9d727b0922137b4626c7bd

Use CRA eslint rules (#25)
Signed-off-by: Jon Harper <jon.harper87@gmail.com>

src/components/AuthenticationRouter/AuthenticationRouter.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import SignInCallbackHandler from '../SignInCallbackHandler';
 import {
@@ -23,6 +23,15 @@ const AuthenticationRouter = ({
     history,
     location,
 }) => {
+    const handleSigninCallbackClosure = useCallback(
+        () => handleSigninCallback(dispatch, history, userManager.instance),
+        [dispatch, history, userManager.instance]
+    );
+    const handleSilentRenewCallbackClosure = useCallback(
+        () =>
+            handleSilentRenewCallback(dispatch, history, userManager.instance),
+        [dispatch, history, userManager.instance]
+    );
     return (
         <React.Fragment>
             {userManager.error !== null && (
@@ -37,20 +46,14 @@ const AuthenticationRouter = ({
                 <Route exact path="/sign-in-callback">
                     <SignInCallbackHandler
                         userManager={userManager.instance}
-                        handleSignInCallback={() =>
-                            handleSigninCallback(
-                                dispatch,
-                                history,
-                                userManager.instance
-                            )
-                        }
+                        handleSignInCallback={handleSigninCallbackClosure}
                     />
                 </Route>
                 <Route exact path="/silent-renew-callback">
                     <SilentRenewCallbackHandler
                         userManager={userManager.instance}
-                        handleSilentRenewCallback={() =>
-                            handleSilentRenewCallback(userManager.instance)
+                        handleSilentRenewCallback={
+                            handleSilentRenewCallbackClosure
                         }
                     />
                 </Route>

demo/src/app.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import TopBar from '../../src/components/TopBar';
 
@@ -48,10 +48,17 @@ const AppContent = () => {
     });
     const [user, setUser] = useState(null);
 
-    let matchSilentRenewCallbackUrl = useRouteMatch({
+    const matchSilentRenewCallbackUrl = useRouteMatch({
         path: '/silent-renew-callback',
         exact: true,
     });
+
+    // Get the routeMatch at page load, so we ignore the exhaustive deps check
+    const initialMatchSilentRenewCallbackUrl = useCallback(
+        () => matchSilentRenewCallbackUrl,
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    )();
 
     const dispatch = (e) => {
         if (e.type === 'USER') {
@@ -62,7 +69,7 @@ const AppContent = () => {
     useEffect(() => {
         initializeAuthenticationDev(
             dispatch,
-            matchSilentRenewCallbackUrl != null
+            initialMatchSilentRenewCallbackUrl != null
         )
             .then((userManager) => {
                 setUserManager({ instance: userManager, error: null });
@@ -72,7 +79,8 @@ const AppContent = () => {
                 setUserManager({ instance: null, error: error.message });
                 console.debug('error when creating userManager');
             });
-    }, []);
+        // Note: initialMatchSilentRenewCallbackUrl doesn't change
+    }, [initialMatchSilentRenewCallbackUrl]);
 
     return (
         <IntlProvider locale={language} messages={messages[language]}>

src/components/SignInCallbackHandler/SignInCallbackHandler.js
@@ -12,7 +12,7 @@ const SignInCallbackHandler = ({ userManager, handleSignInCallback }) => {
         if (userManager !== null) {
             handleSignInCallback();
         }
-    }, [userManager]);
+    }, [userManager, handleSignInCallback]);
 
     return <h1> </h1>;
 };

src/components/SilentRenewCallbackHandler/SilentRenewCallbackHandler.js
@@ -15,7 +15,7 @@ const SilentRenewCallbackHandler = ({
         if (userManager !== null) {
             handleSilentRenewCallback();
         }
-    }, [userManager]);
+    }, [userManager, handleSilentRenewCallback]);
 
     return <h1>Technical token renew window, you should not see this</h1>;
 };

src/components/TopBar/TopBar.test.js
@@ -26,9 +26,9 @@ it('renders', () => {
         render(
             <TopBar
                 appName="DemoApp"
-                onParametersClick={() => console.log('settings')}
-                onLogoutClick={() => logout(dispatch, userManager.instance)}
-                onLogoClick={() => console.log('logo')}
+                onParametersClick={() => {}}
+                onLogoutClick={() => {}}
+                onLogoClick={() => {}}
                 user={null}
             />,
             container

thangqp/commons-ui

commit da6b9a731eefc2c1af1d198a24bb706b1ac007bc

Fix silent login for ProdAuth, regression from f2f06ea375646e6b
Signed-off-by: Jon Harper <jon.harper87@gmail.com>

src/components/AuthenticationRouter/AuthenticationRouter.js
@@ -28,9 +28,8 @@ const AuthenticationRouter = ({
         [dispatch, history, userManager.instance]
     );
     const handleSilentRenewCallbackClosure = useCallback(
-        () =>
-            handleSilentRenewCallback(dispatch, history, userManager.instance),
-        [dispatch, history, userManager.instance]
+        () => handleSilentRenewCallback(userManager.instance),
+        [userManager.instance]
     );
     return (
         <React.Fragment>