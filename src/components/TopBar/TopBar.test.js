46198c66eb4094976006a0ee519a0c46dff707ca
HOMER Etienne <etiennehomer@gmail.com>
2021-03-10 14:27:47 +0100

    Remove useless tests
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/components/TopBar/TopBar.test.js
+++ src/components/TopBar/TopBar.test.js
@@ -41,7 +41,7 @@ it('renders', () => {
                     onParametersClick={() => {}}
                     onLogoutClick={() => {}}
                     onLogoClick={() => {}}
-                    user={{ profile: { name: 'Username' } }}
+                    user={{ profile: { name: 'John Doe' } }}
                     appsAndUrls={apps}
                 >
                     <p>testchild</p>
@@ -53,71 +53,4 @@ it('renders', () => {
     expect(container.textContent).toContain('GridDemotestchildU');
 });
 
-it('renders', () => {
-    act(() => {
-        render(
-            <IntlProvider locale="en" messages={top_bar_en}>
-                <TopBar
-                    appName="Demo"
-                    appColor="#808080"
-                    appLogo={PowsyblLogo}
-                    onParametersClick={() => {}}
-                    onLogoutClick={() => {}}
-                    onLogoClick={() => {}}
-                    user={{ profile: { name: 'John Doe' } }}
-                    appsAndUrls={apps}
-                >
-                    <p>testchild</p>
-                </TopBar>
-            </IntlProvider>,
-            container
-        );
-    });
-    expect(container.textContent).toContain('GridDemotestchildJD');
-});
 
-it('renders', () => {
-    act(() => {
-        render(
-            <IntlProvider locale="en" messages={top_bar_en}>
-                <TopBar
-                    appName="Demo"
-                    appColor="#808080"
-                    appLogo={PowsyblLogo}
-                    onParametersClick={() => {}}
-                    onLogoutClick={() => {}}
-                    onLogoClick={() => {}}
-                    user={{ profile: { name: 'John Jack Doe' } }}
-                    appsAndUrls={apps}
-                >
-                    <p>testchild</p>
-                </TopBar>
-            </IntlProvider>,
-            container
-        );
-    });
-    expect(container.textContent).toContain('GridDemotestchildJD');
-});
-
-it('renders', () => {
-    act(() => {
-        render(
-            <IntlProvider locale="en" messages={top_bar_en}>
-                <TopBar
-                    appName="Demo"
-                    appColor="#808080"
-                    appLogo={PowsyblLogo}
-                    onParametersClick={() => {}}
-                    onLogoutClick={() => {}}
-                    onLogoClick={() => {}}
-                    user={{ profile: { name: '' } }}
-                    appsAndUrls={apps}
-                >
-                    <p>testchild</p>
-                </TopBar>
-            </IntlProvider>,
-            container
-        );
-    });
-    expect(container.textContent).toContain('GridDemotestchild');
-});
