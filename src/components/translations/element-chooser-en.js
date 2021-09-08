538a742b1ab14c5d8547a17271308f0301661d34
sBouzols <sylvain.bouzols@gmail.com>
2021-09-08 16:40:07 +0200

    fix(demo): need to add other case for select format in  Icu syntax

--- src/components/translations/element-chooser-en.js
+++ src/components/translations/element-chooser-en.js
@@ -5,9 +5,9 @@ const element_chooser_en = {
     'element_chooser/add': 'Add...',
     'element_chooser/deleteSelection': 'Delete selection',
     'element_chooser/contentText':
-        '{multiselect, select, true {Please choose one or more element(s).} false {Please choose one element.}}',
+        '{multiselect, select, true {Please choose one or more element(s).} false {Please choose one element.} other {}}',
     'element_chooser/finderTitle':
-        '{multiselect, select, true {Please choose one or more element(s).} false {Please choose one element.}}',
+        '{multiselect, select, true {Please choose one or more element(s).} false {Please choose one element.} other {}}',
     'element_chooser/addElementsValidation':
         '{nbElements, plural, =0 {Please select an element} =1 {Add this element} other{Add # elements}}',
 };
--- src/components/translations/element-chooser-fr.js
+++ src/components/translations/element-chooser-fr.js
@@ -5,9 +5,9 @@ const element_chooser_fr = {
     'element_chooser/add': 'Ajouter...',
     'element_chooser/deleteSelection': 'Supprimer la selection',
     'element_chooser/contentText':
-        '{multiselect, select, true {Veuillez choisir un ou plusieurs éléments.} false {Veuillez choisir un élément.}}',
+        '{multiselect, select, true {Veuillez choisir un ou plusieurs éléments.} false {Veuillez choisir un élément.} other {}}',
     'element_chooser/finderTitle':
-        '{multiselect, select, true {Veuillez choisir un ou plusieurs éléments.} false {Veuillez choisir un élément.}}',
+        '{multiselect, select, true {Veuillez choisir un ou plusieurs éléments.} false {Veuillez choisir un élément.} other {}}',
     'element_chooser/addElementsValidation':
         '{nbElements, plural, =0 {Veuillez sélectionner un élément} =1 {Ajouter cet élément} other {Ajouter # éléments}}',
 };
