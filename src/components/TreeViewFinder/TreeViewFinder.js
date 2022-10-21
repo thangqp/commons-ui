f6701a2a1f0b4e5e038c8c0f118f01b6eb4d98b2
sBouzols <sylvain.bouzols@gmail.com>
2022-10-21 11:56:27 +0200

    fix(TreeViewFinder): Show chevron by default if childrenCount is null or undefinned
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/TreeViewFinder/TreeViewFinder.js
+++ src/components/TreeViewFinder/TreeViewFinder.js
@@ -211,6 +211,10 @@ const TreeViewFinder = (props) => {
             </div>
         );
     };
+    const showChevron = (node) => {
+        // by defaut show Chevron if childrenCount is null or undefined otherwise only if > 0
+        return !!(node.childrenCount == null || (node.childrenCount && node.childrenCount > 0));
+    }
 
     const renderTree = (node) => {
         if (!node) return;
@@ -220,12 +224,12 @@ const TreeViewFinder = (props) => {
                 nodeId={node.id}
                 label={renderTreeItemLabel(node)}
                 expandIcon={
-                    node.childrenCount > 0 ? (
+                    showChevron(node) ? (
                         <ChevronRightIcon className={classes.icon} />
                     ) : null
                 }
                 collapseIcon={
-                    node.childrenCount > 0 ? (
+                    showChevron(node) ? (
                         <ExpandMoreIcon className={classes.icon} />
                     ) : null
                 }
