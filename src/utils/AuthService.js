00179cfd2ac671642fd31d6008162759b9b164fe
sBouzols <sylvain.bouzols@gmail.com>
2022-12-08 12:41:43 +0100

    revert fix of expires_in was set too late during the last renewal, will do in another PR
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -295,16 +295,11 @@ function handleUser(dispatch, userManager, validateUser) {
                         console.log(
                             'Error in silent renew, but idtoken ALMOST expiring (expiring in' +
                                 idTokenExpiresIn +
-                                ') => last chance, next error will logout',
-                            'maxExpiresIn = ' +
+                                ') => last chance' +
                                 userManager.idpSettings.maxExpiresIn,
-                            'last renew attempt in ' +
-                                idTokenExpiresIn -
-                                accessTokenExpiringNotificationTime +
-                                'seconds',
                             error
                         );
-                        user.expires_in = idTokenExpiresIn;
+                        user.expires_in = userManager.idpSettings.maxExpiresIn;
                         userManager.storeUser(user).then(() => {
                             userManager.getUser();
                         });
