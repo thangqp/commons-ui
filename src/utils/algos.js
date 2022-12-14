2bf5e3f2ddf179623cb68dc2c0ddb198f2d45af1
Laurent Garnier <laurent.garnier@rte-france.com>
2022-12-14 16:53:58 +0100

    fix:recursivity of equalsArray

--- src/utils/algos.js
+++ src/utils/algos.js
@@ -12,7 +12,7 @@ export function equalsArray(a, b) {
 
     for (var i = 0, l = a.length; i < l; i++) {
         if (a[i] instanceof Array && b[i] instanceof Array) {
-            if (!a[i].equals(b[i])) return false;
+            if (!equalsArray(a[i], b[i])) return false;
         } else if (a[i] !== b[i]) {
             // Warning - two different object instances will never be equal: {x:20} != {x:20}
             return false;
