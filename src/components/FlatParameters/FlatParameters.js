8e261d4973579731664080b8cde51ddd234e71cd
Laurent Garnier <laurent.garnier@rte-france.com>
2023-04-14 15:03:32 +0200

    fixrev: license headers + console.debug removals

--- src/components/FlatParameters/FlatParameters.js
+++ src/components/FlatParameters/FlatParameters.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import React, { useCallback, useState } from 'react';
 import makeStyles from '@mui/styles/makeStyles';
 import {
@@ -69,12 +76,12 @@ function longestCommonPrefix(strs) {
 }
 
 /**
- *
+ * Present a "list" of independently editable parameters according to
+ * their description, as given by paramsAsArray, with current values as in initValues.
  * @param paramsAsArray [{type,name,possibleValues,defaultValue}]
  * @param initValues {k:v}
  * @param onChange (paramName, newValue, isInEdition)
  * @returns JSX
- * @constructor
  */
 export const FlatParameters = ({ paramsAsArray, initValues, onChange }) => {
     const classes = useStyles();
@@ -164,12 +171,6 @@ export const FlatParameters = ({ paramsAsArray, initValues, onChange }) => {
                         onBlur={() => onUncommited(param, false)}
                         onChange={(e) => {
                             const m = FloatRE.exec(e.target.value);
-                            console.debug(
-                                'double.onChange',
-                                value,
-                                m,
-                                e.target.value
-                            );
                             if (m) {
                                 onFieldChange(e.target.value, param);
                             }
@@ -187,12 +188,6 @@ export const FlatParameters = ({ paramsAsArray, initValues, onChange }) => {
                         onBlur={() => onUncommited(param, false)}
                         onChange={(e) => {
                             const m = IntegerRE.exec(e.target.value);
-                            console.debug(
-                                'double.onChange',
-                                value,
-                                m,
-                                e.target.value
-                            );
                             if (m) {
                                 onFieldChange(e.target.value, param);
                             }
