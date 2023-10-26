91275fff23d7c0aa73bff08118bfc08b0c53ca85
TOURI ANIS <anis-1.touri@rte-france.com>
2023-10-26 18:57:08 +0200

    add custom color

--- src/components/react-hook-form/utils/cancel-button.js
+++ src/components/react-hook-form/utils/cancel-button.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Button } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
 
-const CancelButton = ({ onClick, variant, disabled }) => {
+const styles = {
+    removeButtonTextColor: (theme) => ({
+        color: theme.palette.text.primary,
+    }),
+};
+const CancelButton = ({
+    onClick,
+    variant,
+    disabled,
+    withCustomColor = true,
+}) => {
     return (
         <Button
             onClick={onClick}
             variant={variant}
             disabled={disabled}
-            sx={{ color: 'inherit' }}
+            sx={withCustomColor ? styles.removeButtonTextColor : null}
         >
             <FormattedMessage id="cancel" />
         </Button>
