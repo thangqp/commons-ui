12a3b3c4f2000c8e768783dccee16383d93ee28d
Hugo Marcellin <hugo.marcelin@rte-france.com>
2024-04-11 16:54:46 +0200

    Missing headers

--- src/utils/rhf-inputs/autocomplete-inputs/multiple-autocomplete-input.tsx
+++ src/utils/rhf-inputs/autocomplete-inputs/multiple-autocomplete-input.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { useState } from 'react';
 import { useFieldArray, useWatch } from 'react-hook-form';
 import AutocompleteInput from '../../../components/react-hook-form/autocomplete-input';
--- src/utils/rhf-inputs/range-input.tsx
+++ src/utils/rhf-inputs/range-input.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { useWatch } from 'react-hook-form';
 import FloatInput from '../../components/react-hook-form/numbers/float-input';
 import yup from '../yup-config';
