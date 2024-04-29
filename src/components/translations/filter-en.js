1f2be795cd2669af2bb88fda765482e755e956aa
Florent MILLOT <millotflo@gmail.com>
2024-04-29 15:11:07 +0200

    Added filter translations for filter components.
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

---
+++ src/components/translations/filter-en.js
@@ -0,0 +1,23 @@
+const filter_en = {
+    OR: 'OR',
+    AND: 'AND',
+    rule: 'rule',
+    subGroup: 'subgroup',
+    is: 'is',
+    contains: 'contains',
+    beginsWith: 'begins with',
+    endsWith: 'ends with',
+    exists: 'exists',
+    between: 'between',
+    in: 'in',
+    isPartOf: 'is part of',
+    isNotPartOf: 'is not part of',
+    emptyRule: 'Filter contains an empty field',
+    incorrectRule: 'Filter contains an incorrect field',
+    betweenRule:
+        "Left value of 'between' rule have to be lower than the right value",
+    emptyGroup:
+        'Filter contains an empty group. Consider removing it or adding rules to this group',
+};
+
+export default filter_en;
---
+++ src/components/translations/filter-fr.js
@@ -0,0 +1,23 @@
+const filter_fr = {
+    OR: 'OU',
+    AND: 'ET',
+    rule: 'règle',
+    subGroup: 'sous-groupe',
+    is: 'est',
+    contains: 'contient',
+    beginsWith: 'commence par',
+    endsWith: 'finit par',
+    exists: 'existe',
+    between: 'entre',
+    in: 'dans',
+    isPartOf: 'fait partie de',
+    isNotPartOf: 'ne fait pas partie de',
+    emptyRule: 'Le filtre contient un champ vide',
+    incorrectRule: 'Le filtre contient un champ incorrect',
+    betweenRule:
+        "La valeur de gauche d'une règle 'entre' doit être inférieure à la valeur de droite",
+    emptyGroup:
+        'Le filtre contient un groupe vide. Supprimez le ou ajoutez des règles à ce groupe',
+};
+
+export default filter_fr;
